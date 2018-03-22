a0e863855615ff0b80fd192f793735b2815ede7e
Гришаева Ольга В <grishaevaov@lab227.local>
2018-03-22 14:10:47 +0300

    Test

---
+++ grishaevaov.cpp
@@ -0,0 +1,80 @@
+#include "grishaevaov.h"
+
+/**
+ * Введение в дисциплину
+ */
+void grishaevaov::lab1()
+{
+std::cout<<"Hello, world!";
+}
+
+
+/**
+ * Метод Гаусса с выбором главного элемента
+ */
+void grishaevaov::lab2()
+{
+
+}
+
+
+
+/**
+ * Метод прогонки
+ */
+void grishaevaov::lab3()
+{
+
+}
+
+
+
+/**
+ * Метод простых итераций
+ */
+void grishaevaov::lab4()
+{
+
+}
+
+
+
+/**
+ * Метод Якоби или Зейделя
+ */
+void grishaevaov::lab5()
+{
+
+}
+
+
+
+/**
+ * Метод минимальных невязок
+ */
+void grishaevaov::lab6()
+{
+
+}
+
+
+
+/**
+ * Метод сопряженных градиентов
+ */
+void grishaevaov::lab7()
+{
+
+}
+
+
+void grishaevaov::lab8()
+{
+
+}
+
+
+std::string grishaevaov::get_name()
+{
+  return "Grishaeva O.V.";
+}
